45c7c45a681fb9f4507bff3a01e71b016c907341
Fernando <fernandvillajuana12@gmail.com>
2022-04-13 14:59:36 -0500

    inclusión de php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>The Blockchain Proyect</title>
+        <link rel="stylesheet" type="text/css" href="Estilos/estilo.css">
+    </head>
+    <body>
+        <div class="form">
+            <h1 class="form__h1">The Blockchain Proyect</h1>
+            <h2 class="form__h2">¡Bienvenido Usuario!</h2>
+            <h3 class="form__h3">Porfavor ingrese su matrícula:</h3>
+            <form>
+                <input type="text" placeholder="Matrícula" name="Matrícula">
+                <button>Enviar</button>
+            </form>
+        </div>
+    </body>
+</html>
