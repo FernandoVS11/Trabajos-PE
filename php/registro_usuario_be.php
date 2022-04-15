9736217c8c0ae26e06735e31648b2c3c4b5f3037
Fernando <fernandvillajuana12@gmail.com>
2022-04-15 14:16:08 -0500

    Agregué que no se repitan las matrículas

--- php/registro_usuario_be.php
+++ php/registro_usuario_be.php
@@ -6,6 +6,18 @@
 
     $tabla= "INSERT INTO usuarios(matricula) VALUES('$matricula')";
 
+    $verificar_matricula= mysqli_query($conexion, "SELECT * FROM usuarios WHERE matricula='$matricula'");
+
+    if(mysqli_num_rows($verificar_matricula) > 0){
+        echo '
+            <script>
+                alert("Esta matrícula ya está registrada, ingrese otra diferente");
+                window.location="../pagina_registro.php";
+            </script>
+        ';
+        exit();
+    }
+
     $ejecutar = mysqli_query($conexion, $tabla);    
 
     if($ejecutar){
