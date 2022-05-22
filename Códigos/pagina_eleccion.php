0f9ed96a3c5ae4358d23f206d44c1f404c68bb03
Juan Ceballos <juanca.ceballos@hotmail.com>
2022-05-22 01:54:58 -0500

    Prueba blockchain

--- Códigos/pagina_eleccion.php
+++ Códigos/pagina_eleccion.php
@@ -23,10 +23,38 @@
     </head>
     <body>
         <div class="form">
-            <h1 class="form__h1">Votación pendiente</h1>
+            <h1 class="form__h1">Elija a su candidato</h1>
             <a class= "form_a" href="php/cerrar_sesion.php">Cerrar Sesión</a> 
-            <a class = "candidato 1">PrimerCandidato</a><br>
+            <button id = "candidatoUno">Primer Candidato</button><br>
+            <button id = "candidatoDos">Segundo Candidato</button><br>
+
         </div>
+
+        <script src= "https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js"></script>
+        <script src= "https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin = "anonymous"></script>
+
+        <script>
+            var contract;
+            $(document).ready(function(){
+
+                web3 = new Web3(web3.currentProvider);
+
+                var adress = "";
+                var abi = "";
+
+
+                contract = new web3.eth.Contract(abi, adress);
+
+                contract.methods.verVotos().call().then(function(voto){
+
+                    $('#CandidatoUno').html(voto);
+
+                })
+            })
+
+
+        </script>
+
     </body>
      
 </html>
